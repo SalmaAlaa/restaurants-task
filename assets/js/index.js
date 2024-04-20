29c5f036ddc2041ca6839d1cfede0b8fd47ed010
Salma Alaa <salmaalaa591@gmail.com>
2024-04-20 23:59:32 +0200

    feat: styleing the search form feat: adding JS to handle search form submission

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,12 @@
-'use strict';
+"use strict";
+const searchForm = document.getElementById("search-postcode");
+
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let postcodeInput = document.getElementById("postcode-input");
+  console.log(postcodeInput.value);
+});
+
 const Restaurant = class {
   constructor(name, address, cuisines, rating) {
     this.name = name;
@@ -18,9 +26,7 @@ const Restaurant = class {
     return this.rating.count;
   }
 };
-// Restaurant.prototype.gaddress = function () {
-//   return `${this.address.firstLine}, ${this.address.city}, ${this.address.postalCode}`;
-// };
+
 const getRestaurantsByPostcode = function (postcode) {
   fetch(
     `https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`
@@ -43,21 +49,8 @@ const getRestaurantsByPostcode = function (postcode) {
           cuisines,
           rating
         );
-        console.log(formattedRestaurant);
-        // formattedRestaurant.cuisinesFormatted();
-        //console.log(formattedRestaurant.ratings());
-
-        //console.log(formattedRestaurant.address);
 
         return formattedRestaurant;
       });
-      //   console.log(formattedRestaurants);
-      //   for (const restaurant of formattedRestaurants) {
-      //     // console.log(restaurant);
-      //     // console.log(name, address, cuisines, rating);
-
-      //     console.log(RestaurantInstance);
-      //   }
     });
 };
-getRestaurantsByPostcode('CT1 2EH');
